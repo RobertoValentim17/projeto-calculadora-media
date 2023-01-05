c00a114c0d9ec6b873361c3b38b0046f2c49ae27
Roberto valentim <redevalentim@gmail.com>
2023-01-05 09:41:41 -0300

    add

--- main.js
+++ main.js
@@ -1,14 +1,25 @@
 const form = document.getElementById('form-atividade');
 const imgAprovado = '<img src="./images/aprovado.png" alt="Emoji celebrando" />';
 const imgReprovado = '<img src="./images/reprovado.png" alt="Emoji decepcionado" />';
+const atividade = [];
+const notas = [];
 
 let linhas = '';
 
 form.addEventListener('submit', function(e){
     e.preventDefault();
 
+    adicionaLinha();
+    atualizaTabela();
+    atualizaMediaFinal();
+});
+
+function adicionaLinha(){
     const inputNomeAtividade = document.getElementById('nome-atividade');
     const inputNotaAtividade = document.getElementById('nota-atividade');
+
+    atividade.push(inputNomeAtividade.value);
+    notas.push(parseFloat(inputNotaAtividade.value));
 
     let linha = '<tr>';
     linha += `<td>${inputNomeAtividade.value}</td>`
@@ -18,9 +29,21 @@ form.addEventListener('submit', function(e){
 
     linhas += linha;
 
-    const corpotabela = document.querySelector('tbody');
-    corpotabela.innerHTML = linhas; /* Para inserir um conteudo dentro de uma tag */
-
     inputNomeAtividade.value = '';
     inputNotaAtividade.value = '';
-})
+}
+
+function atualizaTabela () {
+    const corpotabela = document.querySelector('tbody');
+    corpotabela.innerHTML = linhas; /* Para inserir um conteudo dentro de uma tag */
+}
+
+function atualizaMediaFinal () {
+    let somaDasNotas = 0;
+
+    for (let i = 0; i < notas.length; i++ ) {
+        somaDasNotas += notas[i]
+    }
+
+    console.log(atividade);
+}
