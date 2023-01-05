039de7032146c2f333f2557a5f9d587bad11f8e3
Roberto valentim <redevalentim@gmail.com>
2023-01-05 10:11:05 -0300

    add

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const form = document.getElementById('form-atividade');
-const imgAprovado = '<img src="./images/aprovado.png" alt="Emoji celebrando" />';
-const imgReprovado = '<img src="./images/reprovado.png" alt="Emoji decepcionado" />';
-const atividade = [];
+const imgAprovado = '<img src="./imagens/aprovado.png" alt="Emoji celebrando" />';
+const imgReprovado = '<img src="./imagens/reprovado.png" alt="Emoji decepcionado" />';
+const atividades = [];
 const notas = [];
 
 let linhas = '';
@@ -10,22 +10,22 @@ form.addEventListener('submit', function(e){
     e.preventDefault();
 
     adicionaLinha();
-    atualizaTabela();
-    atualizaMediaFinal();
+    atualizarTabela();
+    atualizarMediaFinal();
 });
 
 function adicionaLinha(){
     const inputNomeAtividade = document.getElementById('nome-atividade');
     const inputNotaAtividade = document.getElementById('nota-atividade');
 
-    atividade.push(inputNomeAtividade.value);
+    atividades.push(inputNomeAtividade.value);
     notas.push(parseFloat(inputNotaAtividade.value));
 
     let linha = '<tr>';
     linha += `<td>${inputNomeAtividade.value}</td>`
     linha += `<td>${inputNotaAtividade.value}</td>`
     linha += `<td>${inputNotaAtividade.value >= 7 ? imgAprovado : imgReprovado}</td>`
-    linha += `</tr>`;
+    linha += '</tr>';
 
     linhas += linha;
 
@@ -33,17 +33,17 @@ function adicionaLinha(){
     inputNotaAtividade.value = '';
 }
 
-function atualizaTabela () {
-    const corpotabela = document.querySelector('tbody');
-    corpotabela.innerHTML = linhas; /* Para inserir um conteudo dentro de uma tag */
+function atualizarTabela() {
+    const corpoTabela = document.querySelector('tbody');
+    corpoTabela.innerHTML = linhas; /* Para inserir um conteudo dentro de uma tag */
 }
 
-function atualizaMediaFinal () {
+function atualizarMediaFinal() {
     let somaDasNotas = 0;
 
     for (let i = 0; i < notas.length; i++ ) {
         somaDasNotas += notas[i]
     }
 
-    console.log(atividade);
+    console.log(somaDasNotas);
 }
